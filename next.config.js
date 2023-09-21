1638861dc2685b2923d3a56b635ba2d95150e42b
haeraam <nooroongy@dyphi.com>
2023-09-21 19:44:42 +0900

    gh page test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: "/byenolan",
+  output: "export",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
