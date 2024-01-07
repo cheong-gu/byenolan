1968ad6b478f7d5dd5a25562b9eb0a0f85f7f7d3
gparkkii <jyp933@gmail.com>
2024-01-07 21:43:41 +0900

    결과페이지 렌더링 버그 수정

--- src/app/result/[id]/page.tsx
+++ src/app/result/[id]/page.tsx
@@ -161,7 +161,6 @@ const ModalButton = styled.button`
 interface ResultPageProps {
   params: { id: string };
   searchParams: {
-    type: RelationshipType;
     age: string;
     gender: 'W' | 'M';
     question: string;
@@ -169,9 +168,10 @@ interface ResultPageProps {
   };
 }
 
-export default function ResultPage({ params, searchParams }: ResultPageProps) {
-  const { id } = params;
-  const { type, age, gender } = searchParams;
+export default function ResultPage({
+  params: { id },
+  searchParams: { age, gender, question, answer },
+}: ResultPageProps) {
   const [result, setResult] = useRecoilState(resultState);
   const [modalResult, setModalResult] = useRecoilState(modalResultState);
   const [modalQuestion, setModalQuestion] = useRecoilState(modalQuestionState);
@@ -181,7 +181,6 @@ export default function ResultPage({ params, searchParams }: ResultPageProps) {
 
   const [showModal, setShowModal] = useState<boolean>(false);
   const [showToast, setShowToast] = useState<boolean>(false);
-  const [resultUI, setResultUI] = useState(INITIAL_TYPE_DESIGN);
 
   const resetResult = useResetRecoilState(resultState);
   const resetModalResult = useResetRecoilState(modalResultState);
@@ -211,100 +210,63 @@ export default function ResultPage({ params, searchParams }: ResultPageProps) {
     resetInfoState();
   };
 
-  const getResultType = useCallback(async () => {
+  const fetchData = async (url: string) => {
     try {
-      await fetch(`https://byenolan.shop/surveyResult/result/${id}`)
-        .then((response) => response.json())
-        .then((response) => {
-          setResult(response[0]);
-          console.log('[Result]', response[0]);
-        });
+      const response = await fetch(`https://byenolan.shop/${url}`);
+      return await response.json();
     } catch (e) {
       console.error(e);
     }
-  }, [id, setResult]);
+  };
 
   const getDonutChartData = useCallback(async () => {
-    try {
-      await fetch(
-        `https://byenolan.shop/surveyResult?age=${age}&gender=${gender}`
-      )
-        .then((response) => response.json())
-        .then((response) => {
-          const result = (response as InfoResultType[]).sort(
-            (a, b) => b.count - a.count
-          );
-          const mostType = result[0].title;
-          const myType = result.filter((value) => value.title === type)[0];
-          const chartData = result.map((data) =>
-            parseInt(data.percent.substring(0, data.percent.length - 1))
-          );
-          console.log('[DonutChart]', result, mostType, myType, chartData);
-          setDonutChartData({
-            data: result,
-            donutData: chartData,
-            mostType: mostType,
-            myPercent: parseInt(
-              myType.percent.substring(0, myType.percent.length - 1)
-            ),
-          });
-        });
-    } catch (e) {
-      console.error(e);
-    }
-  }, [age, gender, setDonutChartData, type]);
+    const response = (await fetchData(
+      `surveyResult?age=${age}&gender=${gender}`
+    )) as InfoResultType[];
+    const data = response.sort((a, b) => b.count - a.count);
+    const chartData = data.map((data) =>
+      parseInt(data.percent.substring(0, data.percent.length - 1))
+    );
+    setDonutChartData({
+      data: data,
+      donutData: chartData,
+      mostType: data[0].title,
+    });
+  }, [age, gender, setDonutChartData]);
 
-  const getColumnChartData = useCallback(async () => {
-    try {
-      await fetch(`https://byenolan.shop/surveyResult?title=${type}`)
-        .then((response) => response.json())
-        .then((response) => {
-          const data = (response as InfoResultType[]).sort(
-            (a, b) => b.count - a.count
-          );
-          console.log('[ColumnChart]', data.slice(0, 3));
-          setColumnChartData(data.slice(0, 3));
-        });
-    } catch (e) {
-      console.error(e);
-    }
-  }, [setColumnChartData, type]);
+  const getColumnChartData = useCallback(
+    async (type: string) => {
+      const response = (await fetchData(
+        `surveyResult?title=${type}`
+      )) as InfoResultType[];
+      const data = response.sort((a, b) => b.count - a.count);
+      setColumnChartData(data.slice(0, 3));
+    },
+    [setColumnChartData]
+  );
 
   const getModalResult = useCallback(
     async (result: string[]) => {
-      try {
-        await fetch(`https://byenolan.shop/survey/total/${result}`)
-          .then((response) => response.json())
-          .then((response) => {
-            setModalQuestion(response);
-          });
-      } catch (e) {
-        console.error(e);
-      }
+      const response = await fetchData(`survey/total/${result}`);
+      setModalQuestion(response);
     },
     [setModalQuestion]
   );
 
+  const getResult = useCallback(async () => {
+    const response = await fetchData(`surveyResult/result/${id}`);
+    setResult(response[0]);
+    await getDonutChartData();
+    await getColumnChartData(response[0].title);
+  }, [getColumnChartData, getDonutChartData, id, setResult]);
+
   useEffect(() => {
-    if (id && searchParams) {
-      const question = JSON.parse(decodeURIComponent(searchParams.question));
-      const answer = JSON.parse(decodeURIComponent(searchParams.answer));
-      setResultUI(getTypeDesign(searchParams.type as RelationshipType));
-      setModalResult({ question, answer });
-      void getResultType();
-      void getDonutChartData();
-      void getColumnChartData();
-      void getModalResult(question);
-    }
-  }, [
-    getResultType,
-    getDonutChartData,
-    getColumnChartData,
-    id,
-    searchParams,
-    getModalResult,
-    setModalResult,
-  ]);
+    const decodeQuestion = JSON.parse(decodeURIComponent(question)) as string[];
+    const decodeAnswer = JSON.parse(decodeURIComponent(answer)) as string[];
+    setModalResult({ question: decodeQuestion, answer: decodeAnswer });
+    void getResult();
+    void getModalResult(decodeQuestion);
+  }, [answer, getModalResult, getResult, question, setModalResult]);
 
   const handleModal = () => {
     setShowModal(!showModal);
@@ -323,123 +285,126 @@ export default function ResultPage({ params, searchParams }: ResultPageProps) {
   };
 
   return (
-    <>
-      {showModal && (
-        <Modal isOpen={showModal} onClose={handleModal}>
-          {modalQuestion.map(({ survey, _id }, index) => (
-            <Answer
-              key={_id}
-              index={index + 1}
-              survey={survey}
-              answerList={modalResult.answer}
-            />
-          ))}
-        </Modal>
-      )}
-      <Container typeColor={resultUI.color}>
-        <ResultHeader>
-          <Image src={HeaderImage} alt="result_header" />
-        </ResultHeader>
-        <ContentWrapper>
-          {/* 1. 결과요약 */}
-          <Content index={1}>
-            <ResultImage>
-              <Image src={resultUI.image} alt="type_image" />
-            </ResultImage>
-            <H5>{result.subTitle}</H5>
-            <Margin number={4} />
-            <Stroke
-              title={result.title}
-              typeColor={resultUI.stroke}
-              stroke={5}
-              size="lg"
-            />
-            <BarGraph
-              percentage={result.percentResult}
-              color={resultUI.color}
-            />
-            <Summary>
-              <Body3>{result.content}</Body3>
-            </Summary>
-            <ShareButton onClick={copyToClipBoard}>
-              <Image src={ShareIcon} alt="share" />
-              <H6>결과 공유</H6>
-              <Toast
-                isOpen={showToast}
-                message="링크 복사 완료! 친구에게 공유해 보세요!"
+    donutChartData.data &&
+    columnChartData.length > 0 &&
+    result && (
+      <>
+        {showModal && (
+          <Modal isOpen={showModal} onClose={handleModal}>
+            {modalQuestion.map(({ survey, _id }, index) => (
+              <Answer
+                key={_id}
+                index={index + 1}
+                survey={survey}
+                answerList={modalResult.answer}
               />
-            </ShareButton>
-            <ButtonWrapper>
-              <LinkButton
-                href={'/survey/info'}
-                onClick={resetData}
-                about="repeat"
-              >
-                <Image src={RepeatIcon} alt="repeat" />
-                <H6>다시하기</H6>
-              </LinkButton>
-              <LinkButton href={'/'} onClick={resetData} about="to home">
-                <Image src={HomeIcon} alt="home" />
-                <H6>홈 화면</H6>
-              </LinkButton>
-            </ButtonWrapper>
-          </Content>
-          {/* 2. 사용자 연령대별 유형 */}
-          <Content index={2}>
-            <Row>
+            ))}
+          </Modal>
+        )}
+        <Container typeColor={getTypeDesign(result.title).color}>
+          <ResultHeader>
+            <Image src={HeaderImage} alt="result_header" />
+          </ResultHeader>
+          <ContentWrapper>
+            {/* 1. 결과요약 */}
+            <Content index={1}>
+              <ResultImage>
+                <Image
+                  src={getTypeDesign(result.title).image}
+                  alt="type_image"
+                />
+              </ResultImage>
+              <H5>{result.subTitle}</H5>
+              <Margin number={4} />
               <Stroke
-                title={`${age}대 ${gender === 'W' ? '여성' : '남성'}`}
-                typeColor={resultUI.stroke}
-                stroke={2}
-                size="md"
+                title={result.title}
+                typeColor={getTypeDesign(result.title).stroke}
+                stroke={5}
+                size="lg"
               />
-              <H3>은</H3>
-            </Row>
-            <Margin number={8} />
-            <Row>
-              <Stroke
-                title={donutChartData.mostType}
-                typeColor={resultUI.stroke}
-                stroke={2}
-                size="md"
+              <BarGraph
+                percentage={result.percentResult}
+                color={getTypeDesign(result.title).color}
               />
-              <H3>&nbsp;유형이 가장 많아요</H3>
-            </Row>
-            {donutChartData.data?.length > 0 && (
-              <>
-                <DonutChart type={type} chartData={donutChartData} />
-                {donutChartData.data.map(({ title, percent }, idx) => (
-                  <ChartLegend
-                    key={`${title}_${idx}`}
-                    type={title}
-                    percent={percent}
-                    color={getTypeDesign(title).color}
-                    active={type === title}
-                    lastIndex={idx === donutChartData.data.length}
-                  />
-                ))}
-              </>
-            )}
-          </Content>
-          {/* 3. 사용자 유형별 연령대 */}
-          <Content index={3}>
-            <Row>
-              <Stroke
-                title={type}
-                typeColor={resultUI.stroke}
-                stroke={2}
-                size="md"
-              />
-              <H3>&nbsp;유형이 많은 연령대는?</H3>
-            </Row>
-            <ColumnChart type={type} data={columnChartData} />
-          </Content>
-          <ModalButton onClick={handleModal}>
-            <Image src={AnswerIcon} alt="answers" />
-            <H6>전체 답변 보기</H6>
-          </ModalButton>
-        </ContentWrapper>
-      </Container>
-    </>
+              <Summary>
+                <Body3>{result.content}</Body3>
+              </Summary>
+              <ShareButton onClick={copyToClipBoard}>
+                <Image src={ShareIcon} alt="share" />
+                <H6>결과 공유</H6>
+                <Toast
+                  isOpen={showToast}
+                  message="링크 복사 완료! 친구에게 공유해 보세요!"
+                />
+              </ShareButton>
+              <ButtonWrapper>
+                <LinkButton
+                  href={'/survey/info'}
+                  onClick={resetData}
+                  about="repeat"
+                >
+                  <Image src={RepeatIcon} alt="repeat" />
+                  <H6>다시하기</H6>
+                </LinkButton>
+                <LinkButton href={'/'} onClick={resetData} about="to home">
+                  <Image src={HomeIcon} alt="home" />
+                  <H6>홈 화면</H6>
+                </LinkButton>
+              </ButtonWrapper>
+            </Content>
+            {/* 2. 사용자 연령대별 유형 */}
+            <Content index={2}>
+              <Row>
+                <Stroke
+                  title={`${age}대 ${gender === 'W' ? '여성' : '남성'}`}
+                  typeColor={getTypeDesign(result.title).stroke}
+                  stroke={2}
+                  size="md"
+                />
+                <H3>은</H3>
+              </Row>
+              <Margin number={8} />
+              <Row>
+                <Stroke
+                  title={donutChartData.mostType}
+                  typeColor={getTypeDesign(result.title).stroke}
+                  stroke={2}
+                  size="md"
+                />
+                <H3>&nbsp;유형이 가장 많아요</H3>
+              </Row>
+              <DonutChart type={result.title} chartData={donutChartData} />
+              {donutChartData.data.map(({ title, percent }, idx) => (
+                <ChartLegend
+                  key={`${title}_${idx}`}
+                  type={title}
+                  percent={percent}
+                  color={getTypeDesign(title).color}
+                  active={result.title === title}
+                  lastIndex={idx === donutChartData.data.length}
+                />
+              ))}
+            </Content>
+            {/* 3. 사용자 유형별 연령대 */}
+            <Content index={3}>
+              <Row>
+                <Stroke
+                  title={result.title}
+                  typeColor={getTypeDesign(result.title).stroke}
+                  stroke={2}
+                  size="md"
+                />
+                <H3>&nbsp;유형이 많은 연령대는?</H3>
+              </Row>
+              <ColumnChart type={result.title} data={columnChartData} />
+            </Content>
+            <ModalButton onClick={handleModal}>
+              <Image src={AnswerIcon} alt="answers" />
+              <H6>전체 답변 보기</H6>
+            </ModalButton>
+          </ContentWrapper>
+        </Container>
+      </>
+    )
   );
 }
